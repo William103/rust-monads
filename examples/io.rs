e034292c8dd6a7d719a40a13b92b01dc4da6e9f2
William Ball <williampi103@gmail.com>
2022-05-24 18:18:08 -0700

    initial commit

---
+++ examples/io.rs
@@ -0,0 +1,28 @@
+use monad::{mdo, Applicative, Monad, IO};
+use std::io::Write;
+
+fn get_line() -> IO<String> {
+    let mut str = String::new();
+    let _ = std::io::stdin().read_line(&mut str).expect("something went wrong");
+    IO::pure(str)
+}
+
+fn print(s: String) -> IO<()> {
+    print!("{}", s);
+    std::io::stdout().flush().expect("something went wrong");
+    IO::pure(())
+}
+
+fn main() {
+    let _ = mdo! {
+        let message = "Enter your name: ".to_string();
+        print(message);
+        x <= get_line();
+        if x.len() > 10 {
+            print("Your name is too long!\n".to_string())
+        } else {
+            print("Hello, ".to_string());
+            print(x)
+        }
+    };
+}
